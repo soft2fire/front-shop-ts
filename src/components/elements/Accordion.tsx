18437a8647bf8f9fabda83acacdd745156ba8669
mohammad rahmati <soft2fire@gmail.com>
2021-09-29 14:17:52 +0330

    remove trash line

--- src/components/elements/Accordion.tsx
+++ src/components/elements/Accordion.tsx
@@ -3,9 +3,9 @@ import Typography from '@material-ui/core/Typography';
 import { Accordion, AccordionDetails, AccordionSummary } from './elements.styles';
 
 
-interface accordionProps {
-    name:string,
-}
+// interface accordionProps {
+//     name:string,
+// }
 const Home = () => {
     // const changeTheme = useChangeTheme();
     const [expanded, setExpanded] = React.useState<string | false>('panel1');
@@ -18,7 +18,7 @@ const Home = () => {
         <div>
             <Accordion square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
                 <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
-                    <Typography>Collapsible Group Item #1</Typography>
+                    <Typography>#1 app future</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
@@ -28,7 +28,7 @@ const Home = () => {
             </Accordion>
             <Accordion square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
                 <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
-                    <Typography>Collapsible Group Item #2</Typography>
+                    <Typography># 2security details </Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
@@ -38,7 +38,7 @@ const Home = () => {
             </Accordion>
             <Accordion square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
                 <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
-                    <Typography>Collapsible Group Item #3</Typography>
+                    <Typography>#3 used technology</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
                     <Typography>
